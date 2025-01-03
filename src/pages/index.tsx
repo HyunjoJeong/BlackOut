417ef4918fb6266c26455accf5aeacc79403bc79
hj-Jeong01 <hj.jeong@fanmaum.com>
2025-01-03 15:44:17 +0900

    layout 폴더 구조 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
+import { Footer, Header } from '@/global/layouts';
+
 export default function Home() {
   return (
     <>
+      <Header />
       <main>홈 페이지</main>
+      <Footer />
     </>
   );
 }
