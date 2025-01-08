0a0bf78a2aa2989ef782e949b3695e2393b1134b
ChrisLee02 <hdchris765@snu.ac.kr>
2025-01-08 21:37:31 +0900

    gray 색상 추가

--- src/global/styles/theme.ts
+++ src/global/styles/theme.ts
@@ -1,7 +1,16 @@
 export const theme = {
-  // TODO: 피그마 나오면 변경
   colors: {
-    primary: '#6f42c1',
-    secondary: '#e83e8c',
+    primary: '#033D8D',
+    secondary: '#ECF4FF',
+
+    'gray-0': '#FFFFFF',
+    'gray-100': '#F5F7FA',
+    'gray-200': '#F0F0F5',
+    'gray-300': '#E1E1E8',
+    'gray-400': '#CACCD8',
+    'gray-500': '#ADAFFB',
+    'gray-600': '#858898',
+    'gray-700': '#35363F',
+    'gray-800': '#23252B',
   },
 };
