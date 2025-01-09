f557cfe11c29a885936b8ab0c7b4fc78308198ee
Chrislee02 <hdchris765@snu.ac.kr>
2025-01-09 22:11:06 +0900

    버튼, 칩 폰트 적용용

--- src/core/button/Button.tsx
+++ src/core/button/Button.tsx
@@ -4,15 +4,25 @@ import type { ButtonHTMLAttributes } from 'react';
 import { buttonVariant } from './styles/variant';
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  font?: keyof Theme['typography'];
   variant?: keyof ReturnType<typeof buttonVariant>;
   css?: Interpolation<Theme>;
 };
 
-export const Button = ({ children, variant = 'filledPrimary', css, ...props }: ButtonProps) => {
+export const Button = ({
+  children,
+  variant = 'filledPrimary',
+  font = 'button2',
+  css,
+  ...props
+}: ButtonProps) => {
   const theme = useTheme();
 
   return (
-    <button css={[defaultStyle, buttonVariant(theme)[variant], css]} {...props}>
+    <button
+      css={[defaultStyle, buttonVariant(theme)[variant], theme.typography[font], css]}
+      {...props}
+    >
       {children}
     </button>
   );
--- src/core/chip/Chip.tsx
+++ src/core/chip/Chip.tsx
@@ -5,15 +5,22 @@ import { chipVariant } from './styles/variant';
 
 type ChipProps = HTMLAttributes<HTMLDivElement> & {
   disabled?: boolean;
+  font?: keyof Theme['typography'];
   variant?: keyof ReturnType<typeof chipVariant>;
   css?: Interpolation<Theme>;
 };
 
-export const Chip = ({ children, variant = 'filledPrimary', css, ...props }: ChipProps) => {
+export const Chip = ({
+  children,
+  variant = 'filledPrimary',
+  font = 'button2',
+  css,
+  ...props
+}: ChipProps) => {
   const theme = useTheme();
 
   return (
-    <div css={[defaultStyle, chipVariant(theme)[variant], css]} {...props}>
+    <div css={[defaultStyle, chipVariant(theme)[variant], theme.typography[font], css]} {...props}>
       {children}
     </div>
   );
