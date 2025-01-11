bbd9fae0c685d9601092ccc99f0716e8c279ea3d
Chrislee02 <hdchris765@snu.ac.kr>
2025-01-12 04:34:49 +0900

    주석제거거

--- src/features/featureB/apis/index.ts
+++ src/features/featureB/apis/index.ts
@@ -3,14 +3,6 @@ import { APIServer } from '@/config/axios';
 
 export const fetchEventListData = async () => {
   const { fetchedData }: { fetchedData: EventDto[] } = await APIServer.get('/events/');
-  /* setPinData(
-    fetchedData.map((event) => ({
-      id: Number(event.id),
-      title: event.title,
-      destination: event.destination,
-      coordinates: event.coordinates,
-      }))
-      ); */
   return fetchedData;
 };
 
