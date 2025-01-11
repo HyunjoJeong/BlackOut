7f884578ae9a6f0db5f06de29169c32c780d5eb3
hj-Jeong01 <hj.jeong@fanmaum.com>
2025-01-12 07:40:54 +0900

    feat: google login

---
+++ src/global/utils/index.ts
@@ -0,0 +1,3 @@
+export const initGoogleLogin = () => {
+  window.location.href = 'https://jahayeon-back.fly.dev/api/v1/auth/google/login';
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,17 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 
+// * dayjs
+import dayjs from 'dayjs';
+import timezone from 'dayjs/plugin/timezone';
+import utc from 'dayjs/plugin/utc';
+
+// * 서버에서 저장하는 시간 단위가 utc인데, 프론트 서버 및 클라이언트 pc에서 dayjs가 해당 위치에서의 시간 단위를 사용하기 때문에 dayjs를 그대로 사용하면 오차가 발생하므로 utc plugin을 확장
+// * 서버의 타임존도 한국으로 맞추기 위해 timezone plugin을 확장
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.tz.setDefault('Asia/Seoul');
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
