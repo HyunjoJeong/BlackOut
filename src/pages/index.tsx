6b6cc94e80ab86da402e24175f4d5bcefb47b7c2
hj-Jeong01 <hj.jeong@fanmaum.com>
2025-01-11 18:23:00 +0900

    feat: Header 기초 설정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { Footer, Header } from '@/global/layouts';
 export default function Home() {
   return (
     <>
-      <Header />
+      <Header left={<div>뒤로 가기</div>} center={'타이틀'} right={<div>기타 버튼들</div>} />
       <main>홈 페이지</main>
       <Footer />
     </>
