5c6aa88915a4efdea3fd82bfe04861780c167bc5
hj-Jeong01 <hj.jeong@fanmaum.com>
2025-01-12 01:34:36 +0900

    feat: 목업 데이터 생성 및 대강 화면 구현

--- src/features/map/components/Map.tsx
+++ src/features/map/components/Map.tsx
@@ -1,14 +1,34 @@
 import styled from '@emotion/styled';
 import type { MapItemDto } from '../types/dto';
+import MapPin from './MapPin';
 
 type Props = {
   datas: MapItemDto[];
+  selectedId: number | null;
+  onItemClick: (id: number) => void;
 };
 
-const Map = ({ datas }: Props) => {};
+const Map = ({ datas, selectedId, onItemClick }: Props) => {
+  return (
+    <StyledSection>
+      {datas.map((itemData) => (
+        <MapPin
+          key={itemData.id}
+          data={itemData}
+          isSelected={itemData.id === selectedId}
+          onClick={() => onItemClick(itemData.id)}
+        />
+      ))}
+    </StyledSection>
+  );
+};
 
 export default Map;
 
 const StyledSection = styled.section`
   position: relative;
+  aspect-ratio: 3/4;
+
+  background-size: 100%;
+  background-image: url('/map.png');
 `;
--- src/features/map/constants.ts
+++ src/features/map/constants.ts
@@ -1,1 +1,30 @@
+import type { MapItemDto } from './types/dto';
+
 export const PIN_COLOR = '#FC6813';
+
+export const MOCKUP_DATA: Array<MapItemDto> = [
+  {
+    id: 1,
+    title: '타이틀',
+    destination: '도착지명',
+    coordinates: [40, 120],
+  },
+  {
+    id: 2,
+    title: '타이틀',
+    destination: '도착지명2',
+    coordinates: [60, 220],
+  },
+  {
+    id: 3,
+    title: '타이틀',
+    destination: '도착지명3',
+    coordinates: [100, 340],
+  },
+  {
+    id: 4,
+    title: '타이틀',
+    destination: '도착지명4',
+    coordinates: [240, 170],
+  },
+];
