316d22c525bcb6a14113d78363db422f1afc2123
hj-Jeong01 <hj.jeong@fanmaum.com>
2025-01-12 09:10:47 +0900

    fix: mock data 삭제

--- src/features/meeting/components/MeetingList.tsx
+++ src/features/meeting/components/MeetingList.tsx
@@ -1,7 +1,6 @@
 import ScrollArea from '@/global/components/ScrollArea';
-import { MEET_LIST_MOCKUP } from '../constants';
-import MeetingListItem from './MeetingListItem';
 import { PartyListItemDto } from '../types/dto';
+import MeetingListItem from './MeetingListItem';
 
 type Props = {
   datas: PartyListItemDto[];
